d0cc03b5dc70d46dc6e297c30cf4f10dec30c0c8
Shiv Chauhan <shivchauhan795@gmail.com>
2025-01-04 17:13:35 +0530

    gitignore updation and bug fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 
 function App() {
+  const text = import.meta.env.VITE_TEXT;
   return (
     <>
       <div className='flex flex-col justify-center items-center'>
-        hi
+        <h1>{text}</h1>
       </div>
     </>
   )
