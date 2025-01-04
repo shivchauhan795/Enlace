10db78d2b645e40c2c4ddf39eebb986ec76620f0
Shiv Chauhan <shivchauhan795@gmail.com>
2025-01-04 13:34:53 +0530

    vite app initiated

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        hi
+      </div>
+    </>
+  )
+}
+
+export default App
